338356cb10d89b688fa6501184d970946b38c4f2
Anand Ramdhani <anandkr.github@gmail.com>
2021-02-01 16:39:22 +0200

    Signin and Register

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.json(database.users);
+});
+
+app.listen(3000, () => {
+  console.log("helloha!");
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { email, password, name } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+/*
+root route --> this is working
+/signin --> POST success/fail ...because we posting data or user info we will need to use
+/register --> POST user ...because we want to store the data for future singins etc
+/profile/:userId --> GET = user
+/image --> PUT --> user 
+
+*/
